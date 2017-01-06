24131a889de56398d1ace0d3a7911db35b15ecf1
Austen Johnson <ajohnson9054@gmail.com>
2017-01-06 10:41:42 -0700

    Updated nav to remove components not needed for admins

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -16,13 +16,13 @@ describe 'navigate' do
       visit posts_path
     end
 
-  	it 'can be reached successfully' do
-  		expect(page.status_code).to eq(200)
-  	end
+    it 'can be reached successfully' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'has a title of Posts' do
-  		expect(page).to have_content(/Posts/)
-  	end
+    it 'has a title of Posts' do
+      expect(page).to have_content(/Posts/)
+    end
 
     it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
@@ -43,6 +43,8 @@ describe 'navigate' do
 
   describe 'new' do
     it 'has a link from the homepage' do
+      employee = Employee.create(first_name: 'Non', last_name: 'Authorized', email: "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf", phone: "5555555555")
+      login_as(employee, :scope => :user)
       visit root_path
 
       click_link("new_post_from_nav")
@@ -81,7 +83,7 @@ describe 'navigate' do
       fill_in 'post[overtime_request]', with: 4.5
 
       expect { click_on "Save" }.to change(Post, :count).by(1)
-  	end
+    end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
